b49b8e8dfe205c4f9c6a28e3876db31e3a1ecb68
satoshi <satoshi.kumano@kii.com>
2018-06-06 23:17:16 +0900

    Test inference on jupyter notebook.

---
+++ floydrun-notebook.sh
@@ -0,0 +1,4 @@
+floyd run --cpu --env tensorflow-1.3 \
+    --mode jupyter \
+    --data satoshikumano/datasets/tl-detection-sim-frozen/1:/frozen-graph \
+    --data satoshikumano/datasets/tf-models/2:/tf_models
