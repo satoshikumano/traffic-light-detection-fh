8cf005ba90382eeafa97d0c53d8b83db04f56196
satoshi <satoshi.kumano@kii.com>
2018-07-18 21:24:34 +0900

    Run inference on GPU.
    Floydhub Standard GPU.
    Tesla K80 · 12 GB Memory
    61 GB RAM · 100 GB SSD

--- floydrun-notebook.sh
+++ floydrun-notebook.sh
@@ -1,4 +1,4 @@
-floyd run --cpu --env tensorflow-1.3 \
+floyd run --gpu --env tensorflow-1.3 \
     --mode jupyter \
     --data satoshikumano/datasets/tl-detection-sim-frozen/1:/frozen-graph \
     --data satoshikumano/datasets/tf-models/2:/tf_models
